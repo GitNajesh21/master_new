cd3dfc97d337b385331d5995a829f7130a53cefd
richi <richi@richi-PC>
2021-07-07 13:09:25 +0530

    gg

--- JavaProjects/Git2Project/src/com/services/TestGit.java
+++ JavaProjects/Git2Project/src/com/services/TestGit.java
@@ -3,7 +3,7 @@ package com.services;
 public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Git ...");
-		System.out.println("Updating files ....");
+		System.out.println("Updating files ....***************");
 	}
 
 }
